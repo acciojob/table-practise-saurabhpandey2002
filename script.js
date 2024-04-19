be238c192517357f1dacd15b2f27839d8bdc54a1
Saurabh Pandey <saurabhpandey200200@gmail.com>
2024-04-19 21:04:29 +0530

    ADDD@

--- script.js
+++ script.js
@@ -1,22 +1,6 @@
-//your JS code here. If required.
-let rows = 1;
-
-
 function insert_Row() {
-    let cells = 0;
-  const table = document.querySelector("#sampleTable");
-  const tr = document.createElement("tr");
-  const td1 = document.createElement("td");
-  const td2 = document.createElement("td");
-
-  rows++;
-
-
-  td1.innerText = `Row${rows} cell${cells+1}`;
-
-  td2.innerText = `Row${rows} cell${cells+2}`;
-
-  tr.appendChild(td1);
-  tr.appendChild(td2);
-  table.appendChild(tr);
+    //Write your code here
+	const table = document.getElementById('sampleTable');
+  const newRow = table.insertRow(0); // Insert at position 1 (just after the header row)
+  newRow.insertAdjacentHTML('beforeend', '<td>New Cell1</td><td>New Cell2</td>');
 }
